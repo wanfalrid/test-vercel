68062bbaf6048da4787260873f66fc552ff1ff5f
wanfalrid <152969371+wanfalrid@users.noreply.github.com>
2024-09-22 10:01:43 +0700

    Add files via upload
    makann

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoveLetter from './components/LoveLetter/LoveLetter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LoveLetter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        Untuk pacarku🖤,<br />
+        Sayang, pagi tanpa kamu sarapan itu seperti langit tanpa matahari—terasa hampa. Aku ingin kamu kuat, sehat, dan selalu bersinar, seperti kamu yang selalu menerangi hariku. Jadi, tolong, sebelum dunia ini terasa lebih sepi, sarapanlah... karena hanya dengan begitu, aku tahu kamu menjaga dirimu, dan itu berarti segalanya bagiku. 🌅❤️,<br />
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
