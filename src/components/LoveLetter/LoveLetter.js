f7531c2db88444395a65dc335e9128a1a7560f86
wanfalrid <152969371+wanfalrid@users.noreply.github.com>
2024-09-22 10:06:52 +0700

    Add files via upload

--- src/components/LoveLetter/LoveLetter.js
+++ src/components/LoveLetter/LoveLetter.js
@@ -1,6 +1,6 @@
-import React, { useState, useRef } from 'react';
-import './LoveLetter.css';
-import audioFile from './kushi.mp3';
+import React, { useState, useRef } from "react";
+import "./LoveLetter.css";
+import audioFile from "./Kushi.mp3";
 
 const LoveLetter = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -13,11 +13,12 @@ const LoveLetter = () => {
       setIsFullSize(true);
       // Ensuring audio play is directly a result of this user interaction
       if (audioRef.current) {
-        audioRef.current.play()
+        audioRef.current
+          .play()
           .then(() => console.log("Playback succeeded"))
-          .catch(e => console.error("Playback failed:", e));
+          .catch((e) => console.error("Playback failed:", e));
       }
-    }, 800);
+    }, 0);
   };
 
   const handleCloseLetter = () => {
@@ -27,18 +28,31 @@ const LoveLetter = () => {
         audioRef.current.pause();
       }
       setIsOpen(false);
-    }, 800);
+    }, 0);
   };
 
   return (
-    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+    <div
+      className={`envelope ${isOpen ? "open" : ""}`}
+      onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}
+    >
       <div className="flap"></div>
       <div className="body"></div>
-      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
-        Untuk pacarku🖤,<br />
-        Sayang, pagi tanpa kamu sarapan itu seperti langit tanpa matahari—terasa hampa. Aku ingin kamu kuat, sehat, dan selalu bersinar, seperti kamu yang selalu menerangi hariku. Jadi, tolong, sebelum dunia ini terasa lebih sepi, sarapanlah... karena hanya dengan begitu, aku tahu kamu menjaga dirimu, dan itu berarti segalanya bagiku. 🌅❤️,<br />
+      <div className={`letter ${isFullSize ? "fullSize" : ""}`}>
+        Untuk pacarku🖤,
+        <br />
+        Sayang, pagi tanpa kamu sarapan itu seperti langit tanpa matahari—terasa
+        hampa. Aku ingin kamu kuat, sehat, dan selalu bersinar, seperti kamu
+        yang selalu menerangi hariku. Jadi, tolong, sebelum dunia ini terasa
+        lebih sepi, sarapanlah... karena hanya dengan begitu, aku tahu kamu
+        menjaga dirimu, dan itu berarti segalanya bagiku. 🌅❤️,
+        <br />
       </div>
-      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+      <audio
+        ref={audioRef}
+        src={audioFile}
+        onError={(e) => console.error("Audio error:", e.message)}
+      />
     </div>
   );
 };
